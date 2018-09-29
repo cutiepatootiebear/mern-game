603663cdf02fe8f49891677bd9fc032cfbfabb3c
jameel <jameel.matin@gmail.com>
2018-09-29 16:54:23 -0600

    add userAuth sign up logic

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,25 @@
-const express = require("express")
-const authRouter = express.Router()
-const User = require("../models/user")
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/user");
 
+authRouter.post("/signup", (req, res) => {
+  User.findOne({ username: req.body.username }, (err, existingUser) => {
+    if (err) return res.status(500).send({ success: false, err });
+    // if db doesn't retrun null = there's already a user with the same userName
+    if (existingUser !== null) {
+      return res
+        .status(400)
+        .send({ success: false, err: "Username is already taken" });
+    }
+    // Create new user in DB
+    const newUser = new User(req.body);
+    newUser.save((err, user) => {
+      if (err) return res.status(500).send({ success: false, err });
+      // give user a token
+      const token = jwt.sign(user.toObject(), process.env.SECRET);
+      return res
+        .status(201)
+        .send({ success: false, user: user.toObject(), token });
+    });
+  });
+});
