35750e2b63a308ef3816ac18146d7a65940b2ce3
Justin <justinjwillmore@gmail.com>
2018-10-04 23:47:33 -0600

    added more us states game logic to globe game logic

--- client/src/globe/pages/index.js
+++ client/src/globe/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import {
   ComposableMap,
   ZoomableGlobe,
@@ -12,6 +13,14 @@ import { scaleLinear } from "d3-scale";
 
 import cities from "../static/world-most-populous-cities.json";
 import svgMap from "../static/world-110m.json";
+
+let postsAxios = axios.create();
+
+postsAxios.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 const populationScale = scaleLinear()
   .domain([10750000, 37843000])
@@ -29,9 +38,32 @@ class AlbersUSA extends Component {
     this.state = {
       correctAnswers: [],
       wrongAnswers: [],
+      scores: [],
+      message: "",
       randomState: cities[Math.floor(Math.random() * 30 + 1)]
     };
   }
+
+  componentDidMount() {
+    this.getScores();
+    // setTimeout(this.setHighScore, 200)
+    // axios.get('/states').then(res => {
+    //     let randomNum = Math.floor((Math.random() * 30) + 1)
+    //     this.setState({
+    //         randomState: res.data[randomNum].abbreviation
+    //     })
+    // })
+  }
+
+  getScores = () => {
+    postsAxios.get("/api/scores").then(res => {
+      const mappedData = res.data.map(obj => obj.score);
+      this.setState(prevState => ({
+        scores: [...prevState.scores, ...mappedData],
+        highScore: Math.max(...[...prevState.scores, ...mappedData])
+      }));
+    });
+  };
 
   randomState = () => {
     let randomNum = Math.floor(Math.random() * 30 + 1);
@@ -41,29 +73,73 @@ class AlbersUSA extends Component {
     });
   };
 
+  // mapHandler = event => {
+  //   if (event.name !== this.state.randomState.name) {
+  //     this.setState(prevState => ({
+  //       wrongAnswers: [...prevState.wrongAnswers, "1"]
+  //     }));
+  //     alert("You are incorrect. Try again.");
+  //   } else {
+  //     this.setState(prevState => ({
+  //       correctAnswers: [...prevState.correctAnswers, "1"]
+  //     }));
+  //     alert("Correct!");
+  //     this.randomState();
+  //   }
+  // };
+
   mapHandler = event => {
     if (event.name !== this.state.randomState.name) {
-      this.setState(prevState => ({
-        wrongAnswers: [...prevState.wrongAnswers, "1"]
-      }));
-      alert("You are incorrect. Try again.");
+      if (this.state.wrongAnswers.length > 1) {
+        const newScore = {
+          score: this.state.correctAnswers.length
+        };
+        postsAxios.post("/api/scores", newScore).then(res => {
+          this.gameOver();
+        });
+      } else {
+        this.setState(prevState => ({
+          wrongAnswers: [...prevState.wrongAnswers, "1"]
+        }));
+        alert("You are incorrect. Try again.")
+      }
     } else {
       this.setState(prevState => ({
         correctAnswers: [...prevState.correctAnswers, "1"]
       }));
-      alert("Correct!");
+      alert("Correct! Keep going!");
       this.randomState();
     }
   };
 
+  gameOver = () => {
+    alert("GAME OVER\nScore successfully uploaded");
+    this.setState({
+      wrongAnswers: [],
+      correctAnswers: []
+    });
+  };
+
   render() {
     // this.randomState();
-    console.log(`Wrong answers: ${this.state.wrongAnswers.length}`);
-    console.log(`Correct answers: ${this.state.correctAnswers.length}`);
+    // console.log(`Wrong answers: ${this.state.wrongAnswers.length}`);
+    // console.log(`Correct answers: ${this.state.correctAnswers.length}`);
     return (
       <div style={wrapperStyles}>
         <div className="App">
-          <h1>Try your best at finding {this.state.randomState? this.state.randomState.name : ''}</h1>
+          <h1>
+            Try your best at finding{" "}
+            {this.state.randomState ? this.state.randomState.name : ""}
+          </h1>
+          <div className="flexStuff-globe">
+            <p className="globe-scoreText">Your Highest Score:</p>
+            <p className="globe-highScore">{this.state.highScore}</p>
+            <p className="globe-scoreText2">Score:</p>
+            <p className="globe-score">{this.state.correctAnswers.length}</p>
+            <p className="globe-scoreText2">Incorrect:</p>
+            <p className="globe-wrongAns">{this.state.wrongAnswers.length}</p>
+          </div>
+          {/* <p className="globe-msg">{this.state.message}</p> */}
         </div>
         <ComposableMap
           projection="orthographic"
